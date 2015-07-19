dc681ac5edfa48453757e72ffe46c8a28d2c7eda
Patrick Morris <prmwrites@gmail.com>
2015-07-19 17:57:02 -0400

    So much better! Correct answer, and much easier on the eyes!

--- Euler1.rb
+++ Euler1.rb
@@ -4,43 +4,19 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 # creates an array of all natural numbers below 1000
+
 def array
   (1..999).to_a
 end
 
-# Finds multiples of 3 in our array
-def three_array
-  array.select{|n| (n % 3).zero?}
+# Finds multiples of 3 or 5 in our array
+def three_or_five_array
+  array.select{|n| (n % 3).zero? || (n % 5).zero?}
 end
 
-# Finds multiples of 5 in our array
-def five_array
-  array.select{|n| (n % 5).zero?}
+# adds together the elements of the 'multiples of 3 or 5' array into one sum
+def sum_three_or_five_array
+  three_or_five_array.inject(:+)
 end
 
-# Finds multiples of 15 in our array
-def fifteen_array
-  array.select{|n| (n % 15).zero?}
-end
-
-# adds the elements of the 'multiples of 3' array into one sum
-def sum_three_array
-  three_array.inject(:+)
-end
-
-# adds the elements of the 'multiples of 5' array into one sum
-def sum_five_array
-  five_array.inject(:+)
-end
-
-# adds the elements of the 'multiples of 5' array into one sum
-def sum_fifteen_array
-  fifteen_array.inject(:+)
-end
-
-# adds the two summed arrays together, subtracts extra multiples of 15
-def sum_arrays
-  sum_three_array + sum_five_array - sum_fifteen_array
-end
-
-puts sum_arrays
+puts sum_three_or_five_array
